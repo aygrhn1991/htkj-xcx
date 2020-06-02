1482f1693e2e094bb6c5c997f2d50f1445185fee
aygrhn1991 <411610370@qq.com>
2020-06-02 16:11:22 +0800

    1

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,6 +1,4 @@
-// pages/register/register.js
 Page({
-
   data: {
     userId: null,
     userName: null,
@@ -24,9 +22,6 @@ Page({
     })
   },
   bindSubmit: function () {
-    // wx.redirectTo({
-    //   url: '../index/index',
-    // })
     wx.request({
       url: 'https://wx2.fenglingtime.com/api/addUser',
       method: 'POST',
@@ -43,74 +38,14 @@ Page({
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     wx.request({
       url: 'https://wx2.fenglingtime.com/common/getDepartment',
       success: res => {
-        console.log(res.data);
         this.setData({
           department: res.data
         });
       }
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    wx.hideHomeButton();
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  goHome: function () {
-    wx.redirectTo({
-      url: '../index/index',
-    })
-  },
+  }
 })
