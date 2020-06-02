fc4be2a830b2a824df38754a89905e529e0d397f
aygrhn1991 <411610370@qq.com>
2020-06-02 16:22:34 +0800

    1

--- pages/modules/addjob/addjob.js
+++ pages/modules/addjob/addjob.js
@@ -1,7 +1,7 @@
 var util = require('../../../utils/util');
 Page({
   data: {
-    date: '2021-06-01',
+    date: util.dateToYYYYMMDD(new Date),
     cf: [
       { value: '1', name: '吃饭' },
       { value: '2', name: '不吃饭' },
@@ -12,13 +12,12 @@ Page({
     ]
   },
   bindDateChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e)
     this.setData({
       date: e.detail.value,
     })
   },
 
-  radioChange(e) {
+  bindRadioChange(e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
 
     const items = this.data.items
@@ -32,7 +31,7 @@ Page({
   },
 
   onLoad: function (options) {
-    console.log(util.formatTime(new Date));
+
   },
 
 })
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,32 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':');
 }
-
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+  n = n.toString();
+  return n[1] ? n : '0' + n;
 }
-
+const dateToYYYYMMDDHHMMSS = date => {
+  var y = date.getFullYear();
+  var M = ((date.getMonth() + 1) >= 10 ? '' : '0') + (date.getMonth() + 1);
+  var d = (date.getDate() >= 10 ? '' : '0') + date.getDate();
+  var h = (date.getHours() >= 10 ? '' : '0') + date.getHours();
+  var m = (date.getMinutes() >= 10 ? '' : '0') + date.getMinutes();
+  var s = (date.getSeconds() >= 10 ? '' : '0') + date.getSeconds();
+  return y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s;
+}
+const dateToYYYYMMDD = date => {
+  var y = date.getFullYear();
+  var M = ((date.getMonth() + 1) >= 10 ? '' : '0') + (date.getMonth() + 1);
+  var d = (date.getDate() >= 10 ? '' : '0') + date.getDate();
+  return y + '-' + M + '-' + d;
+}
 module.exports = {
-  formatTime: formatTime
+  dateToYYYYMMDDHHMMSS: dateToYYYYMMDDHHMMSS,
+  dateToYYYYMMDD: dateToYYYYMMDD,
 }
